7f15dcc8ceccd94ad7b752405fe49e47d483128a
Mario Menéndez <mhidalgomario@gmail.com>
2021-04-25 22:58:28 +0200

    feat: Implemented Flower Card

--- src/flower-card.ts
+++ src/flower-card.ts
@@ -26,6 +26,7 @@ import type { BoilerplateCardConfig } from './types';
 import { actionHandler } from './action-handler-directive';
 import { CARD_VERSION } from './const';
 import { localize } from './localize/localize';
+import * as flowers from './flower-card.json';
 
 /* eslint no-console: 0 */
 console.info(
@@ -91,19 +92,66 @@ export class FlowerCard extends LitElement {
     if (!this.config.species) {
       return this._showError(localize('common.show_error'));
     }
+    const state = this.hass.states[this.config.entity];
+    const flower = flowers.db.find(f => f[0] === this.config.species);
+    if (!flower) {
+      return this._showWarning(localize('common.invalid_species'));
+    }
 
-    return html`
-      <ha-card
-        .header=${this.config.name}
-        @action=${this._handleAction}
-        .actionHandler=${actionHandler({
-          hasHold: hasAction(this.config.hold_action),
-          hasDoubleClick: hasAction(this.config.double_tap_action),
-        })}
-        tabindex="0"
-        .label=${`Boilerplate: ${this.config.entity || 'No Entity Defined'}`}
-      ></ha-card>
-    `;
+    const attribute = (icon, val, min, max): TemplateResult => {
+          const pct = 100 * Math.max(0, Math.min(1, (val - min) / (max - min)));
+          return html`
+            <div class="attribute">
+              <ha-icon .icon="${icon}"></ha-icon>
+              <div class="meter red">
+                <span
+                class="${val < min || val > max ? 'bad' : 'good'}"
+                style="width: 100%;"
+                ></span>
+              </div>
+              <div class="meter green">
+                <span
+                class="${val > max ? 'bad' : 'good'}"
+                style="width:${pct}%;"
+                ></span>
+              </div>
+              <div class="meter red">
+                <span
+                class="bad"
+                style="width:${val > max ? 100 : 0}%;"
+                ></span>
+              </div>
+            </div>
+          `;
+    };
+
+      return html`
+        <ha-card>
+          <div class="header" 
+              @action=${this._handleAction}
+              .actionHandler=${actionHandler({
+                hasHold: hasAction(this.config.hold_action),
+                hasDoubleClick: hasAction(this.config.double_tap_action),
+            })}>
+              <img src="/local/flower-card/images/${this.config.species}.jpg">
+              <span id="name"> ${this.config.name || state.attributes.friendly_name}</span>
+              <span id="species"> ${flower[1]} </span>
+          </div>
+
+          <div class="divider"></div>
+
+          <div class="attributes">
+              ${attribute('mdi:thermometer', state.attributes.temperature, flower[2], flower[3])}
+              ${attribute('mdi:white-balance-sunny', state.attributes.brightness, flower[4], flower[5])}
+          </div>
+
+          <div class="attributes">
+              ${attribute('mdi:water-percent', state.attributes.moisture, flower[6], flower[7])}
+              ${attribute('mdi:leaf', state.attributes.conductivity, flower[8], flower[9])}
+          </div>
+
+        </ha-card>
+      `;
   }
 
   private _handleAction(ev: ActionHandlerEvent): void {
@@ -131,7 +179,81 @@ export class FlowerCard extends LitElement {
     `;
   }
 
-  static get styles(): CSSResult {
-    return css``;
-  }
+    static get styles(): CSSResult {
+        return css`
+			ha-card {
+				margin-top: 32px;
+			}
+			.attributes {
+				white-space: nowrap;
+				padding: 8px;
+			}
+			.attribute ha-icon {
+				float: left;
+				margin-right: 4px;
+			}
+			.attribute {
+				display: inline-block;
+				width: 50%;
+				white-space: normal;
+			}
+
+			.header {
+				padding-top: 8px;
+				height: 72px;
+			}
+			.header > img {
+				width: 88px;
+				border-radius: var(--ha-card-border-radius, 2px);
+				margin-left: 16px;
+				margin-right: 16px;
+				margin-top: -32px;
+				float: left;
+				box-shadow: var( --ha-card-box-shadow, 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2) );
+			}
+			.header > #name {
+				font-weight: bold;
+				width: 100%;
+				margin-top: 16px;
+				text-transform: capitalize;
+				display: block;
+			}
+			.header > #species {
+				text-transform: capitalize;
+				color: #8c96a5;
+				display: block;
+			}
+			.meter {
+				height: 8px;
+				background-color: #f1f1f1;
+				border-radius: 2px;
+				display: inline-grid;
+				overflow: hidden;
+			}
+			.meter.red {
+				width: 10%;
+			}
+			.meter.green {
+				width: 50%;
+			}
+			.meter > span {
+				grid-row: 1;
+				grid-column: 1;
+				height: 100%;
+			}
+			.meter > .good {
+				background-color: rgba(43,194,83,1);
+			}
+			.meter > .bad {
+				background-color: rgba(240,163,163);
+			}
+			.divider {
+				height: 1px;
+				background-color: #727272;
+				opacity: 0.25;
+				margin-left: 8px;
+				margin-right: 8px;
+			}
+        `;
+    }
 }
